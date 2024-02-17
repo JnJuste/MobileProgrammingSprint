5894b969d6228d5ef45cafb74ad367fe173e3a00
JnJuste <jeanjusteirakoze@gmail.com>
2024-02-17 21:45:45 +0200

    Internet Connectivity

--- lib/screens/wifi/wifi.dart
+++ lib/screens/wifi/wifi.dart
@@ -1,3 +1,4 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:navigation_bar/screens/drawer/sideMenu.dart';
 
@@ -15,19 +16,89 @@ class _AboutState extends State<Wifi> {
       drawer: SideMenu(),
       appBar: AppBar(
         backgroundColor: Colors.blueAccent,
-        title: Text("Wifi"),
+        title: const Text("Internet Connection"),
+        centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'WIFI',
-              style: TextStyle(fontSize: 40),
-            ),
-          ],
+      body: Container(
+        margin: const EdgeInsets.all(20),
+        width: double.infinity,
+        height: double.infinity,
+        child: StreamBuilder(
+            stream: Connectivity().onConnectivityChanged,
+            builder: (context, AsyncSnapshot<ConnectivityResult> snapshot) {
+              print(snapshot.toString());
+              if (snapshot.hasData) {
+                ConnectivityResult? result = snapshot.data;
+                if (result == ConnectivityResult.mobile) {
+                  // Connected to mobile internet
+                  return connected('Mobile');
+                } else if (result == ConnectivityResult.wifi) {
+                  // Connected to WIFI internet
+                  return connected('WIFI');
+                } else {
+                  // No internet
+                  return noInternet();
+                }
+              } else {
+                // Show that is loading
+                return loading();
+              }
+            }),
+      ),
+    );
+  }
+
+  Widget loading() {
+    return const Center(
+      child: CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+      ),
+    );
+  }
+
+  Widget connected(String type) {
+    return Center(
+      child: Text(
+        "$type Connected",
+        style: const TextStyle(color: Colors.green, fontSize: 20),
+      ),
+    );
+  }
+
+  Widget noInternet() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          'assets/no_internet.png',
+          color: Colors.red,
+          height: 100,
         ),
-      ),
+        Container(
+          margin: const EdgeInsets.only(top: 20, bottom: 10),
+          child: const Text(
+            "No Internet Connection",
+            style: TextStyle(fontSize: 22),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(bottom: 20),
+          child: const Text("Check your Connection, then refresh the page"),
+        ),
+        ElevatedButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.green),
+          ),
+          onPressed: () async {
+            // You can Check Internet Connection if it is back or not through this function
+            ConnectivityResult result =
+                await Connectivity().checkConnectivity();
+            print(result.toString());
+          },
+          child: const Text("Refresh"),
+        ),
+      ],
     );
   }
 }
