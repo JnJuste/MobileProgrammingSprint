16bfac9923866e89f13149bcdeb5a58c3dda3584
JnJuste <jeanjusteirakoze@gmail.com>
2024-02-19 11:53:34 +0200

    Home

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:navigation_bar/screens/drawer/drawer.dart';
-import 'package:navigation_bar/theme_provider.dart';
-import 'package:provider/provider.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -19,11 +17,11 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.pinkAccent,
         title: const Text("Home"),
       ),
-      body: Center(
+      body: const Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
+            Text(
               'HOME',
               style: TextStyle(fontSize: 40),
             ),
